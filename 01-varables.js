b4aa6fb08dc17148ef6e7722fe6c88a5181e85b0
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-04-09 13:09:23 +0530

    variable in js

---
+++ 01-varables.js
@@ -0,0 +1,22 @@
+//variable are like container where we can store values.
+
+
+/*
+we can change the variable type in run time.
+ we can store NUMBER, ARRAY, OBJECT in the same variable, this is called dynamically typed language
+
+ #->Dynamicaly typed language means we can the variable type in runtime.(example- javascript)
+
+ #-> Static type -> we first declare the type of variable, wheather it is a intger, float, or string.
+ */
+
+
+ /**
+  * lets start coding.
+  */
+
+
+let a = 23;
+let b = 32;
+let sum = a + b;
+console.log(sum);
