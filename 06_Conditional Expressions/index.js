8aaae372abc5c0a51dba87a5fd60104d606375f8
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-04-09 16:47:38 +0530

    conditional expressions

---
+++ 06_Conditional Expressions/index.js
@@ -0,0 +1,15 @@
+/**
+ * if-else
+ * if-else-if
+ */
+
+
+//ternary operator
+
+//  condition ? sttement1 : statement2
+
+let age = parseInt(prompt("Enter your age?"));
+
+console.log( age < 18 ? "cannot drive": "can drive")
+
+//run it in html file.
