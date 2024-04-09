e9f62d49fb80c21779c84427a94b250fe3a915ee
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-04-09 16:14:42 +0530

    Assignment Operator

---
+++ 05_assignment Operator/index.js
@@ -0,0 +1,21 @@
+
+console.log(!true) // is false
+
+
+let a = 43;
+let b = 34;
+console.log("a + b is", a + b);
+console.log("a - b is", a - b);
+console.log("a / b is", a / b);
+console.log("a * b is", a * b);
+console.log("a ** b is", a ** b);
+console.log("a % b is", a % b);
+
+
+console.log("a++", a++);
+console.log("a", a);
+console.log("a--", a--);
+console.log("a", a);
+//++a and a++ is almost same both will increment the value of a but in ++a , the value will increment first then it will print. AND same a --(decrement operator)
+console.log("++a", ++a);
+
