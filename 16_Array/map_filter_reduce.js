a8429d9b3521c4198c0dc01fd92d0213324935fd
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-04-11 23:46:59 +0530

    map and filter in js

--- 16_Array/map_filter_reduce.js
+++ 16_Array/map_filter_reduce.js
@@ -3,6 +3,16 @@
  */
 
 let arr = [12,23,34];
-arr.map((value)=>{
-    console.log(value)
+let a = arr.map((value, index, array)=>{  //map is used when we want to create a new array.
+    console.log(value, index, array)
+    return value + 1;
 });
+
+console.log(a)
+
+
+let a1 = [1,2,3,4,5,6,8,7,5,3,3,4,6,6];
+let a2 = a1.filter((a)=> {
+    return a > 4;
+})
+console.log(a2);
