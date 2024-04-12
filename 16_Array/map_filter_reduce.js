dcade4d45ce68bc9bf61dba52f6c48f770ca934d
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-04-12 23:27:03 +0530

    completed map, filter, and reduce in js

--- 16_Array/map_filter_reduce.js
+++ 16_Array/map_filter_reduce.js
@@ -4,15 +4,35 @@
 
 let arr = [12,23,34];
 let a = arr.map((value, index, array)=>{  //map is used when we want to create a new array.
-    console.log(value, index, array)
+    // console.log(value, index, array)
     return value + 1;
 });
 
-console.log(a)
+// console.log(a)
 
 
+
+//filter
 let a1 = [1,2,3,4,5,6,8,7,5,3,3,4,6,6];
 let a2 = a1.filter((a)=> {
     return a > 4;
 })
-console.log(a2);
+// console.log(a2);
+
+
+
+//reduce
+// let arr3 =[1,2,3,4,5,6,43,21,3,4,56];
+// let newarr3 = arr3.reduce((h1,h2) => {
+    //     return h1 + h2
+    // })
+    // console.log(newarr3)
+    
+    //another method
+    
+let arr3 =[1,2,3,4,5,6,43,21,3,4,56];
+const reduce_func = (h1, h2) => {
+    return h1 + h2
+}
+let newarr3 = arr3.reduce(reduce_func);
+console.log(newarr3);
