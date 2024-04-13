51abe83f4fac0b3453c608044da198ed592b5d5c
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-04-13 09:53:40 +0530

    practice problem on Map, filter and reduce

--- 17_Array_Exercise/index.js
+++ 17_Array_Exercise/index.js
@@ -2,3 +2,14 @@
  * Q:create an array of number and take input from the user to add number to this array.
  */
 
+//in jacascript the reduce function is  used to reduce an array into a single value
+
+//the syntax for reduce() is:
+array.reduce(function(total, currentValue, currentIndex, arr), initialValue)
+
+
+//random question solution
+const number = [1,2,3,4,5];
+const sum = number.reduce((total, current) => total + current, 0);
+console.log(sum);
+
