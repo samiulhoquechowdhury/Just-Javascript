3f30431cadd3c90c9891d51d8bd5212019f6ca30
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-04-15 00:32:23 +0530

    then and catch.

---
+++ 17_Promises/.then,.catch/script.js
@@ -0,0 +1,29 @@
+
+let p = new Promise((resolve, reject) => {
+    console.log("Promise is pending.");
+    setTimeout( () => {
+        alert("I am a promise and i am rejected.")
+        resolve(true)
+    }, 3000)
+});
+
+console.log(p);
+
+
+let p2 = new Promise((resolve, reject) => {
+    console.log("Promise is pending.");
+    setTimeout( () => {
+        alert("I am a promise and i am rejected.")
+        reject(new Error("I am an Error."))
+    }, 5000)
+});
+
+// console.log(p2);
+
+p1.then((value) =>{
+    console.log(value);
+});
+
+p2.catch((error) =>{
+    console.log("Some error are there in p2.")
+})
