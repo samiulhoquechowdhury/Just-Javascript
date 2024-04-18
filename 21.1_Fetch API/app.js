ef035b7809eef780ecda27975f658283c686885d
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-04-18 21:19:57 +0530

    Using fetch API to get random cat facts.

--- 21.1_Fetch API/app.js
+++ 21.1_Fetch API/app.js
@@ -4,3 +4,21 @@
 
 //syntax -> let promise = fetch(url, [option])
 
+
+const URL = "https://cat-fact.herokuapp.com/facts";
+const factPara = document.querySelector("#facts");
+const btn = document.querySelector("#btn");
+
+
+const getFacts = async  () => {
+	console.log('getting data...')
+	let response = await fetch(URL);
+	console.log(response);
+	let data = await response.json(); 
+	// console.log(data[2].text);
+	factPara.innerText = data[1].text
+}
+btn.addEventListener("click", getFacts)
+
+
+
