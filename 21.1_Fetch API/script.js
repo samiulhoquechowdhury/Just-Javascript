7bcdaf06461224853053fad511e5ae0a97966dd4
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-04-18 20:42:15 +0530

    fetch API

---
+++ 21.1_Fetch API/script.js
@@ -0,0 +1,5 @@
+//the Fetch API provides an interface for fetching(sendind/receiving) resources.
+//It uses Request and Response objects.
+//? The fetch() method is used to fetch a resourse (data).
+
+//syntax -> let promise = fetch(url, [option])
