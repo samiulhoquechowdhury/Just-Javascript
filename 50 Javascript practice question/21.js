5eabd66ac1e42ef823b9f2e6fc1f99c3a77ef3ce
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-08-05 17:12:50 +0530

    initial commit

--- 50 Javascript practice question/21.js
+++ 50 Javascript practice question/21.js
@@ -1,25 +1,24 @@
-//factorial
+factorial
 
-// function factorial(n) {
-//     if (n === 1) {
-//         return 1;
-//     }
-//     else {
-//         return n * factorial(n - 1)
-//     }
-// }
-// console.log(factorial(5));
+function factorial(n) {
+    if (n === 1) {
+        return 1;
+    }
+    else {
+        return n * factorial(n - 1)
+    }
+}
+console.log(factorial(5));
 
 
 //factorial using arrow function
 
-const factorial = (num) => {
+const fact = (num) => {
     if (num === 1) {
         return 1;
     }
     else {
-        return num * factorial(num - 1)
+        return num * fact(num - 1)
     }
 }
-
-console.log(factorial(5));
+console.log(fact(6));
