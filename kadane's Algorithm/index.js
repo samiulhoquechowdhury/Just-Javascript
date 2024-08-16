d5303684021c6d9ea62642a319ec33c1b2ff1301
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-08-17 00:34:40 +0530

    kadane's algo

--- kadane's Algorithm/index.js
+++ kadane's Algorithm/index.js
@@ -8,10 +8,9 @@
 
 function maxSubArray(nums) {
     let maxSum = nums[0];
-
     for (let i = 0; i < nums.length; i++) {
         let currentSum = 0;
-        for (j = i; j < nums.length; j++) {
+        for (let j = i; j < nums.length; j++) {
             currentSum = currentSum + nums[j];
             if (currentSum > maxSum) {
                 maxSum = currentSum;
@@ -20,6 +19,4 @@ function maxSubArray(nums) {
     }
     return maxSum;
 }
-console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])
-);
-
+console.log(maxSubArray([5,4,-1,7,8]));
