86d61682bb0b5e60c30554259763d497bd6082bf
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-08-16 23:57:02 +0530

    kadane algo

---
+++ kadane's Algorithm/index.js
@@ -0,0 +1,25 @@
+//Implementation of kadanes algorithm
+
+
+//Given an intezer array nums, find the subarray with the kargest sum, and return its sum
+
+//Input:-- [-2,1,-3,4,-1,2,1,-5,4] ======>>> Output: 6, [4,-1,2,1]
+//Input:-- [5,4,-1,7,8]            ======>>> Output: 23, [5,4,-1,7,8]
+
+function maxSubArray(nums) {
+    let maxSum = nums[0];
+
+    for (let i = 0; i < nums.length; i++) {
+        let currentSum = 0;
+        for (j = i; j < nums.length; j++) {
+            currentSum = currentSum + nums[j];
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+        }
+    }
+    return maxSum;
+}
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+);
+
