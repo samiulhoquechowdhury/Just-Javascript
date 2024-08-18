ce922583d1360634f9654890899f4958ab7290e0
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-08-19 00:12:27 +0530

    dsa

--- DSA/SumOfNaturalNumber.js
+++ DSA/SumOfNaturalNumber.js
@@ -2,6 +2,20 @@
  * sum of natural numebr from 1 to 10
  */
 
-function sumOfNaturalNumer(){
-    
+function sumOfNaturalNumbers(num) {
+    let sum = 0;
+    for (let i = 0; i <= num; i++) {
+        sum = sum + i;
+    }
+    return sum;
 }
+let result = sumOfNaturalNumbers(5);
+console.log(result);
+
+//we can also implement it by using formula ======>>>>   n * n + 1
+
+function sum(num){
+    let sum = num * num + 1
+    console.log(sum)
+}
+sum(5);
